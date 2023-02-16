d617be34991188f61ddf35c0d750088abf0e4f7e
rpau <raquel@moderne.io>
2023-02-16 17:28:37 +0100

    Reemplacing parameters for env vars·

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh -l
 
+export MODERNE_PUBLISH_URL=$1
+export MODERNE_PUBLISH_USER=$2
+export MODERNE_PUBLISH_PWD=$3
+export MODERNE_GRADLE_PLUGIN_VERSION=$4
+export MODERNE_MVN_PLUGIN_VERSION=$5
+export MODERNE_MVN_SETTINGS_XML=$6
+export MODERNE_ACTIVE_STYLE=$7
 export JAVA_HOME=/usr/lib/jvm/java-$8-openjdk
-
+export MODERNE_BUILD_ACTION=$9
 # shellcheck disable=SC2039
 
-exec /usr/share/moderne/moderne-cli publish --url $1 --user $2 --password $3 --gradlePluginVersion $4 --mvnPluginVersion $5 --mvnSettingsXml $6  --activeStyle $7  --path /github/workspace
+exec /usr/share/moderne/moderne-cli publish  --path /github/workspace
 
